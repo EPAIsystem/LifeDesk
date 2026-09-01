fb291515a5d1f0e671e3d49640e14ab86283d04a
EPAIsystem <emapanigh@gmail.com>
2026-09-01 21:04:51 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// LifeDesk Service Worker v7 — cache bust
-const CACHE = 'lifedesk-v7';
+// LifeDesk Service Worker v11 — cache bust
+const CACHE = 'lifedesk-v11';
 const ASSETS = ['/', '/index.html'];
 
 self.addEventListener('install', function(e) {
